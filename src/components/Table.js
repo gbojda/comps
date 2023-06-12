a33dcdc5bb740882325017227e33ecb2fdfb07cc
gbbojda <bojdait@gmail.com>
2023-06-12 19:07:45 +0200

    11
    Table added

---
+++ src/components/Table.js
@@ -0,0 +1,36 @@
+function Table({ data, config }) {
+    const renderedHeaders = config.map((column) => {
+       return <th key={column.label}>{column.label}</th>;
+    });
+
+const renderedRows = data.map((fruit) => {
+    const renderedCells = config.map((column) => {
+         return (
+         <td className="p-2" key={column.label}>{column.render(fruit)}</td>
+         );
+    });
+    
+    
+    return (
+     <tr className="border-b" key={fruit.name}>
+       {renderedCells}
+     </tr>
+    );
+});
+
+    return (
+     <table className="table-auto border-spacing-2">
+        <thead>
+            
+            <tr className="border-b-2">
+                <th>Fruit</th>
+                <th>Color</th>
+                <th>Score</th>
+            </tr>
+        
+        </thead>
+        <tbody>{renderedRows}</tbody>
+    </table>
+    );
+}
+export default Table;
