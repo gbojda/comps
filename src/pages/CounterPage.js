f42284c39db4cc8a8abf66dd902b3336a6e8cc91
gbbojda <bojdait@gmail.com>
2023-06-13 15:43:39 +0200

    13

---
+++ src/pages/CounterPage.js
@@ -0,0 +1,15 @@
+import Button from '../components/Button';
+import useCounter from '../hooks/use-counter';
+
+function CounterPage ({ initialCount }) {
+  const { count, Increment } = useCounter(initialCount);
+
+  return ( 
+  <div>
+    <h1>Count is{count}</h1>
+    <Button onClick={Increment}>Increment</Button>
+  </div>
+  );
+}
+
+export default CounterPage;
